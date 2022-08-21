0eb820b0b89458d37d2a16ca711df26f87a1a2d8
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-08-21 18:10:46 -0400

    Added some constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
+use std::path::PathBuf;
+
+use chrono::Utc;
+
 pub mod core;
+
+pub const NAME: &str = "timecard";
+pub const AUTHOR: &str = "Stephen-Hamilton-C";
+
+pub fn data_dir() -> PathBuf {
+    appdirs::user_data_dir(Some(NAME), Some(AUTHOR), false)
+        .expect("Unknown error while retrieving data directory.")
+}
+
+pub fn config_dir() -> PathBuf {
+    appdirs::user_config_dir(Some(NAME), Some(AUTHOR), false)
+        .expect("Unknown error while retrieving config directory.")
+}
+
+pub fn timecard_path() -> PathBuf {
+    data_dir().join(format!("timecard_{}.json", Utc::now().date_naive().to_string()))
+}
