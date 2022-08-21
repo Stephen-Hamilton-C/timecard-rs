dc9a1042369301c14304c16eaeb6f0b7910744c6
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2022-08-21 18:11:00 -0400

    Implemented TimeEntries

--- src/core.rs
+++ src/core.rs
@@ -1,57 +1,28 @@
 use chrono::naive::NaiveDateTime;
 use json::object;
+use json::JsonValue;
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
 use std::process;
+
+fn data_error(msg: &str) -> ! {
+	eprintln!("{msg} Is your timecard file corrupt?");
+	process::exit(1);
+}
 
 #[cfg(test)]
 mod test;
 
 #[derive(Debug)]
+#[derive(Clone)]
 pub struct TimeEntry {
-    pub start: NaiveDateTime,
-    pub end: Option<NaiveDateTime>,
+	pub start: NaiveDateTime,
+	pub end: Option<NaiveDateTime>,
 }
 
 impl TimeEntry {
-    pub fn serialize(&self) -> String {
-        let start_timestamp = self.start.timestamp();
-        let end_timestamp: Option<i64> = match self.end {
-            Some(datetime) => Some(datetime.timestamp()),
-            None => None,
-        };
-
-        let data = object! {
-            start: start_timestamp,
-            end: end_timestamp,
-        };
-
-        println!("{:#}", data);
-
-        data.dump()
-    }
-
-    pub fn from(json: String) -> TimeEntry {
-        let data = json::parse(&json).unwrap_or_else(|_| {
-			deserialize_error("Data parse error!");
-        });
-
-        if data["start"].is_null() {
-            deserialize_error("Start time cannot be null!");
-        }
-
-		let start_data = data["start"].as_i64().unwrap_or_else(|| {
-			deserialize_error("Start time isn't a number!");
-		});
-
-		let end_data = data["end"].as_i64();
-		
-		let start = NaiveDateTime::from_timestamp(start_data, 0);
-		let end = match end_data {
-			Some(data) => Some(NaiveDateTime::from_timestamp(data, 0)),
-			None => None,
-		};
-
-		TimeEntry { start, end }
-    }
 }
 
 impl PartialEq for TimeEntry {
@@ -75,7 +46,107 @@ impl PartialEq for TimeEntry {
 	}
 }
 
-fn deserialize_error(msg: &str) -> ! {
-    eprintln!("{msg} Is your timecard file corrupt?");
-    process::exit(1);
+impl Into<JsonValue> for TimeEntry {
+	fn into(self) -> JsonValue {
+		let start_timestamp = self.start.timestamp();
+		let end_timestamp: Option<i64> = match self.end {
+			Some(datetime) => Some(datetime.timestamp()),
+			None => None,
+		};
+
+		let data = object! {
+			start: start_timestamp,
+			end: end_timestamp,
+		};
+
+		println!("{:#}", data);
+
+		data
+	}
+}
+
+impl From<&JsonValue> for TimeEntry {
+	fn from(data: &JsonValue) -> Self {
+		if data["start"].is_null() {
+			data_error("Start time cannot be null!");
+		}
+
+		let start_data = data["start"].as_i64().unwrap_or_else(|| {
+			data_error("Start time isn't a number!");
+		});
+
+		let end_data = data["end"].as_i64();
+		
+		let start = NaiveDateTime::from_timestamp(start_data, 0);
+		let end = match end_data {
+			Some(data) => Some(NaiveDateTime::from_timestamp(data, 0)),
+			None => None,
+		};
+
+		TimeEntry { start, end }	
+	}
+}
+
+pub struct TimeEntries {
+	pub entries: Vec<TimeEntry>,
+}
+
+impl TimeEntries {
+	pub fn new() -> TimeEntries {
+		TimeEntries { entries: Vec::new() }
+	}
+
+	pub fn save(self) -> Result<(), Box<dyn Error>> {
+		let timecard_path = crate::timecard_path();
+		let mut timecard = File::create(timecard_path)?;
+		
+		let data: JsonValue = self.into();
+		timecard.write_all(data.dump().as_bytes())?;
+
+		Ok(())
+	}
+
+	pub fn load() -> TimeEntries {
+		let timecard_path = crate::timecard_path();
+		let mut timecard = File::create(timecard_path)
+			.expect("Error opening timecard. Is your timecard file corrupt?");
+
+		let mut data = String::new();
+		timecard.read_to_string(&mut data)
+			.expect("Error reading timecard. Is your timecard file corrupt?");
+		
+		let parsed_data = json::parse(&data)
+			.expect("Unable to parse JSON. Is your timecard file corrupt?");
+
+		TimeEntries::from(&parsed_data)
+	}
+}
+
+impl Into<JsonValue> for TimeEntries {
+	fn into(self) -> JsonValue {
+		let mut data = object! {
+			entries: [],
+		};
+		for entry in self.entries {
+			data["entries"].push(entry)
+				.expect("Should be impossible for entries to not be array.");
+		}
+
+		data
+	}
+}
+
+impl From<&JsonValue> for TimeEntries {
+	fn from(data: &JsonValue) -> Self {
+		if !data["entries"].is_array() {
+			data_error("Entries is not an array!");
+		}
+		let entries_data = data["entries"].members();
+		let mut entries: Vec<TimeEntry> = Vec::new();
+		for entry in entries_data {
+			entries.push(TimeEntry::from(entry));
+		}
+		
+		TimeEntries { entries }
+	}
 }
--- src/core/test.rs
+++ src/core/test.rs
@@ -1,4 +1,4 @@
-use chrono::{Duration, Utc};
+use chrono::{Duration, Utc, SubsecRound};
 use std::ops::Sub;
 
 use super::*;
@@ -10,11 +10,11 @@ fn timeentry_serializes() {
         start: start.clone(),
         end: None,
     };
-    let entry_data = entry.serialize();
+    let entry_data: JsonValue = entry.into();
     let expected_data = object! {
         start: start.timestamp(),
         end: null,
-    }.dump();
+    };
 
     assert_eq!(entry_data, expected_data);
 }
@@ -25,14 +25,13 @@ fn timeentry_deserializes() {
     let entry_data = object! {
         start: start.timestamp(),
         end: null,
-    }.dump();
-    let entry = TimeEntry::from(entry_data);
+    };
+    let entry = TimeEntry::from(&entry_data);
     let expected_entry = TimeEntry { start, end: None };
 
     assert_eq!(entry, expected_entry);
 }
 
-/*
 #[test]
 fn timeentries_serializes() {
     let now = Utc::now().naive_local();
@@ -45,8 +44,8 @@ fn timeentries_serializes() {
         end: None,
     };
 
-    let entries = TimeEntries::new(vec![entry1, entry2]);
-    let entries_data = timeEntries.serialize();
+    let entries = TimeEntries { entries: vec![entry1.clone(), entry2.clone()] };
+    let entries_data: JsonValue = entries.into();
     let expected_data = object! {
         entries: [
             {
@@ -58,13 +57,39 @@ fn timeentries_serializes() {
                 end: null,
             },
         ]
-    }.dump();
+    };
 
     assert_eq!(entries_data, expected_data);
 }
 
 #[test]
 fn timeentries_deserializes() {
+    let now = Utc::now().naive_local().round_subsecs(0);
+    let entry1 = TimeEntry {
+        start: now.sub(Duration::minutes(30)),
+        end: Some(now.sub(Duration::minutes(15))),
+    };
+    let entry2 = TimeEntry {
+        start: now.sub(Duration::minutes(5)),
+        end: None,
+    };
 
+    let entries_data = object! {
+        entries: [
+            {
+                start: entry1.start.timestamp(),
+                end: entry1.end.unwrap().timestamp(),
+            },
+            {
+                start: entry2.start.timestamp(),
+                end: null,
+            },
+        ]
+    };
+    let entries = TimeEntries::from(&entries_data);
+
+    assert_eq!(entries.entries[0].start, entry1.start);
+    assert_eq!(entries.entries[0].end, entry1.end);
+    assert_eq!(entries.entries[1].start, entry2.start);
+    assert_eq!(entries.entries[1].end, entry2.end);
 }
-*/
