4b508538eeb9c5c3c9735dae6cfb97ae752c81fd
Stephen Hamilton <stephen.c.hamilton@protonmail.com>
2026-03-07 13:32:34 -0500

    Show log on successful undo

--- bin/src/commands/undo.rs
+++ bin/src/commands/undo.rs
@@ -1,17 +1,31 @@
+use chrono::{DateTime, Local};
 use timecard::Timecard;
 
-use crate::{AppPaths, traits::{Loadable, Saveable}};
+use crate::{AppPaths, commands::{self, LogArgs}, traits::{Loadable, Saveable}};
 
 
 pub fn undo(paths: &AppPaths) {
     // TODO: expect
     let mut timecard = Timecard::load(&paths.timecard).expect("Failed to load timecard");
 
+    let mut undo_day: Option<DateTime<Local>> = None;
+    if let Some(last_time) = timecard.entries().last() {
+        undo_day = Some(last_time.end.unwrap_or(last_time.start));
+    }
+
     // TODO: expect
     timecard.undo().expect("Failed to undo");
     // TODO: expect
     timecard.save(&paths.timecard).expect("Failed to save Timecard");
 
-    // TODO: show status
-    println!("Undid last entry");
+    println!("Successfully removed last entry.");
+    commands::log(
+        &LogArgs {
+            from_date: None,
+            to_date: None,
+            day: undo_day,
+            all: undo_day.is_none(),
+        },
+        paths,
+    );
 }
